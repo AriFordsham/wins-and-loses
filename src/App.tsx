c6806b89b358cd3ad6a5bf3b3b4814b779f95f06
AriFordsham <37623136+AriFordsham@users.noreply.github.com>
2025-11-12 13:45:17 +0000

    Switch to reactrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+// import { createClient } from '@supabase/supabase-js'
+
 import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+import Signup from './Signup'
 
 function App() {
+  // const supabase = createClient('https://rfgnubwzhqzpkttafojn.supabase.co','sb_publishable_gLq9mShgW_mQu6NDFGdN1g_K_i8sBxt')
   // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Check In</h1>
-    </>
+    <BrowserRouter>
+      <Signup />
+    </BrowserRouter>
   )
 }
 
---
+++ src/Signup.tsx
@@ -0,0 +1,9 @@
+function Signup() {
+  return (
+    <>
+      <h1>You clicked the link!</h1>
+    </>
+  )
+}
+
+export default Signup
